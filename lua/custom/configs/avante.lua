656d2d77a9ffad194e06656053a80f6dd425a116
Roland Tannous <rolandtannous@gonovel.co>
2024-12-27 16:23:54 +0000

    updated avante config; added openrouter with deepseek as main provider

--- lua/custom/configs/avante.lua
+++ lua/custom/configs/avante.lua
@@ -1,11 +1,66 @@
 local opts = {
-provider = "claude", -- Recommend using Claude
-  auto_suggestions_provider = "claude", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+provider = "openai", -- Openrouter with deepseek as primary provider. Claude as secondary
+  auto_suggestions_provider = "openai", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
   claude = {
     endpoint = "https://api.anthropic.com",
     model = "claude-3-5-sonnet-20241022",
     temperature = 0,
-    max_tokens = 4096,
+    max_tokens = 8192,
   },
-}
+  openai = {
+    endpoint = "https://openrouter.ai/api/v1",
+    model = "deepseek/deepseek-chat",
+    api_key_name = "OPENROUTER_API_KEY_AVANTE",
+    temperature = 0.0,
+    max_tokens = 8000,
+  },
+    behaviour = {
+    auto_suggestions = false, -- Experimental stage
+    auto_set_highlight_group = true,
+    auto_set_keymaps = true,
+    auto_apply_diff_after_generation = true,
+    support_paste_from_clipboard = false,
+    minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
+  },
+  hints = { enabled = true },
+  windows = {
+    position = "right", -- the position of the sidebar
+    wrap = true, -- similar to vim.o.wrap
+    width = 30, -- default % based on available width
+    sidebar_header = {
+      enabled = true, -- true, false to enable/disable the header
+      align = "center", -- left, center, right for title
+      rounded = true,
+    },
+    input = {
+      prefix = "> ",
+      height = 8, -- Height of the input window in vertical layout
+    },
+    edit = {
+      border = "rounded",
+      start_insert = true, -- Start insert mode when opening the edit window
+    },
+    ask = {
+      floating = false, -- Open the 'AvanteAsk' prompt in a floating window
+      start_insert = true, -- Start insert mode when opening the ask window
+      border = "rounded",
+      focus_on_apply = "ours", -- which diff to focus after applying
+    },
+  },
+  highlights = {
+    diff = {
+      current = "DiffText",
+      incoming = "DiffAdd",
+    },
+  },
+  diff = {
+    autojump = true,
+    list_opener = "copen",
+    --- Override the 'timeoutlen' setting while hovering over a diff (see :help timeoutlen).
+    --- Helps to avoid entering operator-pending mode with diff mappings starting with `c`.
+    --- Disable by setting to -1.
+    override_timeoutlen = 500,
+  },
+
+  }
 return opts
